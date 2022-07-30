0dcc3995621039428aaceb8ef334d7bdbc4bb017
Christian Widlund <christian.widlund@gmail.com>
2022-07-30 11:41:42 +0200

    move db route to handlers

--- services/api/cmd/vinyl_catalog/handlers/handlers.go
+++ services/api/cmd/vinyl_catalog/handlers/handlers.go
@@ -1,12 +1,43 @@
 package handlers
 
 import (
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
 	"io"
 	"net/http"
+	"os"
+	"time"
 )
 
 func UpHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, `{"status": "I'm up!"}`)
+}
+
+func DbHandler(w http.ResponseWriter, r *http.Request) {
+	config := mysql.NewConfig()
+	config.User = os.Getenv("DB_USER_NAME")
+	config.Passwd = os.Getenv("DB_USER_PASSWORD")
+	config.Net = "tcp"
+	config.Addr = fmt.Sprintf("%s:%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
+	config.DBName = os.Getenv("DB_SCHEMA_NAME")
+	config.Params = map[string]string{"charset": "utf8"}
+	config.Loc = time.UTC
+	config.ParseTime = true
+	db, err := sql.Open("mysql", config.FormatDSN())
+	if err != nil {
+		fmt.Fprintf(w, "Error when opening connection: %s", err.Error())
+		return
+	}
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	err = db.Ping()
+	if err != nil {
+		fmt.Fprintf(w, "Error when pinging database: %s", err.Error())
+		return
+	}
+	fmt.Fprint(w, "Successfully reached AWS Database!")
 }
