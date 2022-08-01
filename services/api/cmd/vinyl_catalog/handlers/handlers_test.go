2e833e91d5494a5ff3e53d6aed39f9480d99dbfc
Christian Widlund <christian.widlund@gmail.com>
2022-08-01 23:20:59 +0200

    status

--- services/api/cmd/vinyl_catalog/handlers/handlers_test.go
+++ services/api/cmd/vinyl_catalog/handlers/handlers_test.go
@@ -33,7 +33,7 @@ func TestUpHandler(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `{"status": "I'm up!"}`
+	expected := `{"status":"I'm up!"}`
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
